567c9a88e37fab0937333f16409d9e83586ed201
Rodrigo Junior <Rodrigo Junior>
2017-05-31 17:01:56 -0300

    Acertei os metodos, aparerentemente está tudo de acordo com a tecnica

--- trabalho_hash/trabalho_hash/Program.cs
+++ trabalho_hash/trabalho_hash/Program.cs
@@ -9,14 +9,15 @@ namespace trabalho_hash
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> buscarVocalulario()
         {
             List<string> vocabulario = new List<string>();
+
             string[] arquivos = new string[] { "1", "2", "3", "4", "5", "6" };
             int maiorPalavra = 0;
             foreach (var arquivo in arquivos)
             {
-                StreamReader f = new StreamReader(@"D:\Projetos\trabalho_hash\trabalho_hash\arquivos\" + arquivo + ".txt");
+                StreamReader f = new StreamReader(@"D:\Projetos\trabalhos_tap\trabalho_hash\trabalho_hash\arquivos\" + arquivo + ".txt");
                 String texto = f.ReadToEnd();
                 texto = texto.Replace(System.Environment.NewLine, ",");
                 texto = texto.Replace(".", "");
@@ -43,22 +44,94 @@ namespace trabalho_hash
             }
             vocabulario.Sort();
 
-            FileStream fs = new FileStream(@"D:\Projetos\trabalho_hash\trabalho_hash\arquivos\vocabulario.txt", FileMode.Create, FileAccess.Write);
+            return vocabulario;
+        }
+
+        static int buscarMaiorLength(List<string> vocabulario)
+        {
+            int maiorLength = 0;
+
+            foreach (string palavra in vocabulario)
+                if (palavra.Length > maiorLength)
+                    maiorLength = palavra.Length;
+
+            return maiorLength;
+        }
+
+        static int[] calcularPesos(int tamanho)
+        {
+            int[] pesos = new int[tamanho];
+            Random rand = new Random();
+            for (int i = 0; i < tamanho; i++)
+            {
+                pesos[i] = rand.Next(1, tamanho);
+            }
+
+            return pesos;
+        }
+
+        static int calcularK(string palavra, int[] pesos)
+        {
+            int k = 0;
+            for (int i = 0; i < palavra.Length; i++)
+                k += palavra[i] * pesos[i];
+
+            return k;
+        }
+
+        static string[] gerarTabelaHash()
+        {
+            List<string> vocabulario = buscarVocalulario();
+            int tamanhoMaximo = buscarMaiorLength(vocabulario);
+            int[] pesos = calcularPesos(tamanhoMaximo);
+
+            string[] tabelaHash = new string[vocabulario.Count];
+
+            foreach (string palavra in vocabulario)
+            {
+                int k = calcularK(palavra, pesos);
+                int h, i = 0;
+                bool colidiu = false;
+                do
+                {
+                    h = (k + i) % tabelaHash.Length;
+                    if (tabelaHash[h] != null)
+                    {
+                        colidiu = true;
+                        if (i > tabelaHash.Length - 1)
+                            i = 0;
+                        else
+                            i++;
+                    }
+                    else
+                    {
+                        colidiu = false;
+                        tabelaHash[h] = palavra;
+                    }
+                } while (colidiu);
+            }
+
+            return tabelaHash;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            FileStream fs = new FileStream(@"D:\Projetos\trabalhos_tap\trabalho_hash\trabalho_hash\arquivos\vocabulario.txt", FileMode.Create, FileAccess.Write);
 
             StreamWriter file = new StreamWriter(fs, ASCIIEncoding.ASCII);
 
             string pesos = "";
-            Random rand = new Random();
-            for (int i = 0; i < maiorPalavra; i++)
+            foreach (var peso in calcularPesos(buscarMaiorLength(buscarVocalulario())))
             {
-                pesos += rand.Next(1, maiorPalavra) + " ";
+                pesos += peso + " ";
             }
 
-            pesos = pesos.Substring(0, pesos.Length - 2);
+            file.WriteLine(pesos.Trim());
 
-            file.WriteLine(pesos);
-
-            foreach (string palavra in vocabulario)
+            foreach (string palavra in gerarTabelaHash())
             {
                 file.WriteLine(palavra);
             }
